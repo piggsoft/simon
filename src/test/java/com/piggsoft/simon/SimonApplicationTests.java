46bdb91164eb581fbca73150e1b3ea318edc0c24
piggsoft <piggsoft@163.com>
2017-04-04 22:01:29 +0800

    修改文档上的某些配置错误，使之能正常实现显示约束条件

--- src/test/java/com/piggsoft/simon/SimonApplicationTests.java
+++ src/test/java/com/piggsoft/simon/SimonApplicationTests.java
@@ -1,5 +1,6 @@
 package com.piggsoft.simon;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
 import org.junit.Before;
 import org.junit.Rule;
 import org.junit.Test;
@@ -7,12 +8,24 @@ import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.restdocs.JUnitRestDocumentation;
+import org.springframework.restdocs.constraints.ConstraintDescriptions;
+import org.springframework.restdocs.mockmvc.RestDocumentationResultHandler;
+import org.springframework.restdocs.payload.FieldDescriptor;
+import org.springframework.restdocs.request.ParameterDescriptor;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+import org.springframework.util.StringUtils;
 import org.springframework.web.context.WebApplicationContext;
 
+import static org.springframework.restdocs.mockmvc.MockMvcRestDocumentation.document;
 import static org.springframework.restdocs.mockmvc.MockMvcRestDocumentation.documentationConfiguration;
+import static org.springframework.restdocs.operation.preprocess.Preprocessors.preprocessRequest;
+import static org.springframework.restdocs.operation.preprocess.Preprocessors.preprocessResponse;
+import static org.springframework.restdocs.operation.preprocess.Preprocessors.prettyPrint;
+import static org.springframework.restdocs.payload.PayloadDocumentation.fieldWithPath;
+import static org.springframework.restdocs.request.RequestDocumentation.parameterWithName;
+import static org.springframework.restdocs.snippet.Attributes.key;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
@@ -22,15 +35,25 @@ public class SimonApplicationTests {
 	public JUnitRestDocumentation restDocumentation =
 			new JUnitRestDocumentation("target/generated-snippets");
 
+	private RestDocumentationResultHandler documentationHandler;
+
 	private MockMvc mockMvc;
 
 	@Autowired
 	private WebApplicationContext context;
 
+	@Autowired
+	private ObjectMapper objectMapper;
+
 	@Before
 	public void setUp() {
+		this.documentationHandler = document("{method-name}",
+				preprocessRequest(prettyPrint()),
+				preprocessResponse(prettyPrint()));
+
 		this.mockMvc = MockMvcBuilders.webAppContextSetup(this.context)
 				.apply(documentationConfiguration(this.restDocumentation))
+				.alwaysDo(this.documentationHandler)
 				.build();
 	}
 
@@ -40,5 +63,47 @@ public class SimonApplicationTests {
 
 	public MockMvc getMockMvc() {
 		return mockMvc;
+	}
+
+	public static class BodyConstrainedFields {
+
+		private final ConstraintDescriptions constraintDescriptions;
+
+		public BodyConstrainedFields(Class<?> input) {
+			this.constraintDescriptions = new ConstraintDescriptions(input);
+		}
+
+		public FieldDescriptor withPath(String path) {
+			return fieldWithPath(path).attributes(key("constraints").value(StringUtils
+					.collectionToDelimitedString(this.constraintDescriptions
+							.descriptionsForProperty(path), ". ")));
+		}
+	}
+
+	public static class ParamsConstrainedFields {
+
+		private final ConstraintDescriptions constraintDescriptions;
+
+		public ParamsConstrainedFields(Class<?> input) {
+			this.constraintDescriptions = new ConstraintDescriptions(input);
+		}
+
+		public ParameterDescriptor withName(String name) {
+			return parameterWithName(name)
+					.attributes(
+							key("constraints")
+									.value(StringUtils.collectionToDelimitedString(
+											this.constraintDescriptions.descriptionsForProperty(name), ". ")
+									)
+					);
+		}
+	}
+
+	public ObjectMapper getObjectMapper() {
+		return objectMapper;
+	}
+
+	public RestDocumentationResultHandler getDocumentationHandler() {
+		return documentationHandler;
 	}
 }
--- src/test/java/com/piggsoft/simon/admin/controller/SiteControllerDocumentation.java
+++ src/test/java/com/piggsoft/simon/admin/controller/SiteControllerDocumentation.java
@@ -1,12 +1,17 @@
 package com.piggsoft.simon.admin.controller;
 
 import com.piggsoft.simon.SimonApplicationTests;
+import com.piggsoft.simon.admin.vo.SiteVo;
 import org.junit.Test;
 import org.springframework.http.MediaType;
 
 import static org.springframework.restdocs.mockmvc.MockMvcRestDocumentation.document;
+import static org.springframework.restdocs.payload.PayloadDocumentation.fieldWithPath;
+import static org.springframework.restdocs.payload.PayloadDocumentation.responseFields;
+import static org.springframework.restdocs.request.RequestDocumentation.requestParameters;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
 
 /**
  * Created by piggs on 2017/4/2.
@@ -15,11 +20,17 @@ public class SiteControllerDocumentation extends SimonApplicationTests {
 
     @Test
     public void registerSite() throws Exception {
+
+        ParamsConstrainedFields fields = new ParamsConstrainedFields(SiteVo.class);
+
         getMockMvc().perform(post("/admin/site")
                 .param("siteName", "哈哈")
                 .accept(MediaType.APPLICATION_JSON))
                 .andExpect(status().isOk())
-                .andDo(document("/index"));
+                .andDo(getDocumentationHandler().document(
+                        requestParameters(fields.withName("siteName").description("网站名称")),
+                        responseFields(
+                        fieldWithPath("siteName").description("The user's contact details"))));
     }
 
 }
