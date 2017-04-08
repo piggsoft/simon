7e6c52b18fe1c890d5aba4d39ee5e00106ed0f06
piggsoft <piggsoft@163.com>
2017-04-08 11:33:06 +0800

    add new branch

--- src/main/java/com/piggsoft/simon/SimonApplication.java
+++ src/main/java/com/piggsoft/simon/SimonApplication.java
@@ -2,9 +2,19 @@ package com.piggsoft.simon;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.security.oauth2.client.EnableOAuth2Sso;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.security.Principal;
 
 @SpringBootApplication
+@EnableOAuth2Sso
 public class SimonApplication {
+
+	@RequestMapping("/user")
+	public Principal user(Principal principal) {
+		return principal;
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(SimonApplication.class, args);
